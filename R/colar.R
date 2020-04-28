f1519930a7b39656c74420554413218c0b5e12e0
gaugc <gacampos@voegol.com.br>
2020-04-27 22:00:12 -0300

    Nova função colar

---
+++ R/colar.R
@@ -0,0 +1,14 @@
+#' Cola dois textos
+#'
+#' Recebe um texto x e outro y e retorna a cola de x e y
+#'
+#' @param x primeiro argumento
+#' @param y segundo arumento
+#'
+#' @return a cola de dois textos
+#'
+#' @export
+#'
+colar <- function(x, y) {
+  paste(x,y)
+}
